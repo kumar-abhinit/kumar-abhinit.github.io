9f4279838c33f472bcb42ebbde90d9a6796f4357
Abhinit Kumar <kumarabhinit.dev@gmail.com>
2026-01-20 02:31:58 +0530

    Update next.config.ts to add basePath and assetPrefix for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '/kumar-abhinit.github.io',
+  assetPrefix: '/kumar-abhinit.github.io/'
 };
 
 module.exports = nextConfig;
